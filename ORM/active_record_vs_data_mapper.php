50a58b7f7a9829ac25e39587236fa3e181014292
Dragos <Dragos.Dragos@honeywell.com>
2019-10-17 12:50:17 +0300

    test

---
+++ ORM/active_record_vs_data_mapper.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+    orig-src: https://www.thoughtfulcode.com/orm-active-record-vs-data-mapper/
+
+    ### ORM Patterns: The Trade-Offs of Active Record and Data Mappers for Object-Relational Mapping
+
+
+        What is an Object-Relation Mapper (ORM)?
+
+            In order to iteract with a relational DB in an OOP fashion, we need to 
+            create and store objects in it and the ORM is a system that allows us to 
+            do that (create and store) and to retrieve them from the DB
+
+            Among ORMs, there are a two very common philosophies or patterns: 
+
+                Active Record 
+                    
+                    and 
+                    
+                Data Mapper.
+
+
+        Benefits of using an ORM
+
+            Real entities you can manipulate and pass around in your code.
+
+                ! database entities = 
+                
+                An entity is any object in the system that we 
+                want to model and store information about. The entitiy is the conceptual
+                representation of a table, the table is the physical construct in the database
+
+            less SQL/less boilerplate
+
+        Downsides of ORM
+
+            Less SQL/boilerplate code can be more confusing
+
+
+
+
+
+         #### ACTIVE RECORD
+
+            kepp notes from here...
+
+
+*/
